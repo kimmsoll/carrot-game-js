29d2a6cd53b91d515d12c1dbdde4d6eaf71e6f7b
kimmsoll <kimmsoll@naver.com>
2021-09-11 19:40:06 +0900

    change main.js
    js 로직 수정

--- main.js
+++ main.js
@@ -1,93 +1,120 @@
-'use strict'
+"use strict"
 
 const body = document.querySelector("body");
-const popUp = body.querySelector(".pop");
+const popUp = body.querySelector(".pop-up");
 const gameBtn = document.querySelector(".game-btn");
-const timer = document.querySelector(".timer");
-let sec = 10;
+const timer = document.querySelector(".game-timer");
+const gameField = document.querySelector(".game-field");
+let remains = document.querySelector(".game-remains");
 
 const CARROT_SIZE = 80;
-const gameField = document.querySelector(".images");
-const fieldRect = gameField.getBoundingClientRect();
+let sec = 10;
+let remainNum = 10;
+let clock;
+let started = false;
 
-let remains = document.querySelector(".remains");
-let remainNum = 15;
-
-function initGame(){
-  addItem('carrot', 15, 'images/carrot.png');
-  addItem('bug', 15, 'images/bug.png');
+const popUpLost = () => {
+  popUp.setAttribute("id", "pop-up");
+  popUp.innerHTML = `<span>You Lost!💩</span>
+  <button class="pop-up-btn">
+  <i class="fas fa-reply" onClick="window.location.reload()"></i>
+  </button>`;
 }
 
-function randomNum(min, max){
+const popUpWin =() => {
+  popUp.setAttribute("id", "pop-up");
+  popUp.innerHTML = `<span>You Win!🎉</span>
+  <button class="pop-up-btn">
+  <i class="fas fa-reply" onClick="window.location.reload()"></i>
+  </button>`;
+}
+
+const popUpReplay = () => {
+  popUp.setAttribute("id", "pop-up");
+  popUp.innerHTML = `<span>Replay?😀</span>
+  <button class="pop-up-btn">
+  <i class="fas fa-reply" onClick="window.location.reload()"></i>
+  </button>`;
+}
+
+const randomNum = (min, max) => {
   return Math.random()*(max - min) + min;
 }
 
-function popUpLost(){
-  popUp.setAttribute("id", "pop");
-  popUp.innerHTML = `<span>You Lost!💩</span>
-  <i class="fas fa-reply" onClick="window.location.reload()"></i>`
+const initGame = () => {
+  addItem("carrot", 10, "images/carrot.png");
+  addItem("bug", 10, "images/bug.png");
 }
 
-function popUpWin(){   
-  popUp.setAttribute("id", "pop");
-  popUp.innerHTML = `<span>You Win!🎉</span>
-  <i class="fas fa-reply" onClick="window.location.reload()"></i>`
-}
-
-function popUpReplay(){
-  popUp.setAttribute("id", "pop");
-  popUp.innerHTML = `<span>Replay?😀</span>
-  <i class="fas fa-reply" onClick="window.location.reload()"></i>`
-}
-
-function addItem(className, count, imgPath){
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = fieldRect.width;
-  const y2 = fieldRect.height;
+const addItem = (className, count, imgPath) => {
+  const fieldRect = gameField.getBoundingClientRect();
   for(let i = 0; i<count; i++){
-    const item = document.createElement('img');
-    item.setAttribute('class', className);
-    item.setAttribute('src', imgPath);
-    item.style.position = 'absolute';
-    const x = randomNum(x1, x2) - CARROT_SIZE;
-    const y = randomNum(y1, y2) - CARROT_SIZE;
+    const y1 = 0;
+    const x1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+    const item = document.createElement("img");
+    item.setAttribute("class", className);
+    item.setAttribute("src", imgPath);
+    item.style.position = "absolute";
+    const x = randomNum(x1, x2);
+    const y = randomNum(y1, y2);
     item.style.left = `${x}px`;
     item.style.top = `${y}px`;
-    gameField.appendChild(item); 
-    item.addEventListener('click', ()=>{
-      if(item.className === 'carrot'){
-      gameField.removeChild(item);
-      remainNum--;
-      remains.innerHTML = `${remainNum}`
-      if(remainNum === 0){
-        popUpWin();
-      }
-    }
-      else{       
-        popUpLost();       
-    }
+    gameField.appendChild(item);
+    item.addEventListener("click", (e) => {
+      removeItems(e);
     });
   }
 }
 
-gameBtn.addEventListener('click', ()=>{setInterval(()=>{
-  if(gameBtn.innerHTML = `<i class="fas fa-play"></i>`){  
-      gameBtn.innerHTML = `<i class="fas fa-stop" onClick="popUpReplay()"></i>`;     
-      if(sec === 10){
-        initGame();
-        timer.innerHTML = '00'+':'+`${sec}`;
-        sec--;
-    } else if(sec > 0 && sec !== 10){
-        timer.innerHTML = '00'+':'+`0${sec}`;
-        sec--;
-    } else if(sec === 0){
-      timer.innerHTML = '00:00';
-      if(remainNum !== 0){
-      popUpLost(); 
-      }
+const removeItems = (e) => {
+  if(e.target.className === "carrot"){
+    gameField.removeChild(e.target);
+    remainNum--;
+    remains.innerHTML = `${remainNum}`;
+    if(remainNum === 0 && sec > 0){
+      stopGame();
+      popUpWin();
     }
-    }},1000)    
-  });
+  } else if(e.target.className === "bug"){
+    stopGame();
+    popUpLost();
+  }
+}
 
+const startGame = () => {
+  clock = setInterval(() => {
+    if (sec <= 0) {
+      stopGame();
+      popUpLost();
+    } else {
+      --sec;
+      timer.innerHTML = sec<10 ? `00:0${sec}` : `00:${sec}`;
+    }
+  }, 1000);
+  gameBtn.innerHTML = `<i class="fas fa-stop"></i>`;
+}
 
+const stopGame = () => {
+  if(sec > 0) {
+    popUpReplay();
+  }
+  clearInterval(clock);
+  gameBtn.innerHTML = `<i class="fas fa-play"></i>`;
+  gameField.innerHTML = "";
+  sec = 10;
+  remainNum = 10;
+  remains.innerHTML = remainNum;
+  timer.innerHTML = sec<10 ? `00:0${sec}` : `00:${sec}`;
+}
+
+gameBtn.addEventListener("click", () => {
+  if(!started){
+    startGame();
+    initGame();
+  }else{
+    stopGame();
+  }
+  started = !started;
+})
